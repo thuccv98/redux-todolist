0f07d4983d42a32db5c3d44853e66ea55f7937f5
thuccv98 <thuccv98@gmail.com>
2021-08-25 16:45:36 +0700

    Create reducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import TodoReducer from './reducers/TodoReducers';
 
 const reducer = combineReducers({
   // this containes all reducers
+  Todo: TodoReducer,
 });
 
 const initialState = {};
